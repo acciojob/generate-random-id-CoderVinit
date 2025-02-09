5ec26c0566a1ce7a4cd05a192cf2ea7573021ecf
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-09 17:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	let result = "";
+	let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+	for(let i=0;i<l;i++){
+		result+=char.charAt(
+			Math.floor(Math.random()*char.length)
+		)
+	}
+	return result
 }
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l));   
